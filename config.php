9e023fb8bbc1e4ac690e9f4842c50598312e4b44
kuwa1 <kuromu36@yahoo.co.jp>
2020-01-05 00:12:11 +0900

    first commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+ini_set("display_errors", 1);
+
+define("DSN", "mysql:host=localhost;dbname=to_do_php;charset=utf8;unix_socket=/tmp/mysql.sock");
+define("DB_USERNAME", "dbuser");
+define("DB_PASSWORD", "kuwa");
+
+?>
